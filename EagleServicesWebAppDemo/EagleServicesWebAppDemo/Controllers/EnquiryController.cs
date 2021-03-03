0349e0d354ba9ee9852fd9a0765bfc2013da489f
ThuYeinHtun <72776150+ThuYeinHtun@users.noreply.github.com>
2021-03-03 16:52:23 +0630

    Add enquiry view for part with filter

---
+++ EagleServicesWebAppDemo/EagleServicesWebAppDemo/Controllers/EnquiryController.cs
@@ -0,0 +1,80 @@
+﻿using EagleServicesWebApp.Components;
+using EagleServicesWebApp.Models.Main;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EagleServicesWebApp.Controllers
+{
+    public class EnquiryController : Controller
+    {
+        // GET: Enquiry
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetEngineList()
+        {
+            MainModel oClass = new MainModel();
+            var vResultdata = oClass.GetEngineList().ToList();
+
+            return Json(vResultdata, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetModuleList(int engineID)
+        {
+            MainModel oClass = new MainModel();
+            var vResultdata = oClass.GetModuleList().Where(e => e.EngineID == engineID).ToList();
+
+            return Json(vResultdata, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetInspectionList()
+        {
+            MainModel oClass = new MainModel();
+            var vResultdata = oClass.GetInspectionStatusList().ToList();
+
+            return Json(vResultdata, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Part_Read([DataSourceRequest] DataSourceRequest poRequest, int? enginID, int? moduleID, int? inspectionStatus, string partName)
+        {
+            string condition = ""; string joinStr;
+            if (enginID != null)
+                condition += " where engineID=" + enginID.Value;
+            if (moduleID != null)
+            {
+                joinStr = enginID != null ? " and " : " where ";
+                condition += joinStr
+                              + " moduleID=" + moduleID.Value;
+            }
+            if (inspectionStatus != null)
+            {
+                joinStr = (enginID != null || moduleID != null) ? " and " : " where ";
+                condition += joinStr
+                              + " inspectionStatusID=" + inspectionStatus.Value;
+            }
+            if (!string.IsNullOrEmpty(partName))
+            {
+                joinStr = (enginID != null || moduleID != null || inspectionStatus != null) ? " and " : " where ";
+                condition += joinStr
+                              + " PartName like '%" + partName + "%'";
+            }
+            MainModel oClass = new MainModel();
+            System.Collections.Generic.List<Part_Enquiry> vResult = null;
+            try
+            {
+                int _actualPSize = poRequest.PageSize;
+                int _actualPageNo = poRequest.Page;
+                if (moduleID != 0)
+                    vResult = oClass.GetPartDataByCondition(condition).ToList();
+            }
+            catch (Exception ex)
+            {
+                GlobalFunction.SendErrorToText(ex);
+            }
+            return new JsonResult() { Data = vResult.ToDataSourceResult(poRequest), JsonRequestBehavior = JsonRequestBehavior.AllowGet, MaxJsonLength = Int32.MaxValue };
+        }
+    }
+}
--- EagleServicesWebAppDemo/EagleServicesWebAppDemo/Models/Main/MainModel.cs
+++ EagleServicesWebAppDemo/EagleServicesWebAppDemo/Models/Main/MainModel.cs
@@ -21,14 +21,14 @@ namespace EagleServicesWebApp.Models.Main
             return attribute == null ? value.ToString() : attribute.Description;
         }
     }
-    public enum InspectionStatus
-    {
-        Servicable = 1,
-        [Description("External Repair")]
-        //[EnumMember(Value = "External Repair")]
-        ExternalRepair,
-        Scrap
-    }
+    //public enum InspectionStatus
+    //{
+    //    Servicable = 1,
+    //    [Description("External Repair")]
+    //    //[EnumMember(Value = "External Repair")]
+    //    ExternalRepair,
+    //    Scrap
+    //}
     public enum ProcessStatus
     {
         RFIDTagAssociation = 1,
@@ -50,7 +50,27 @@ namespace EagleServicesWebApp.Models.Main
     {
         public int ModuleID { get; set; }
         public string ModuleName { get; set; }
+        public int EngineID { get; set; }
         public string EngineName { get; set; }
+    }
+    public class Part
+    {
+        public int PartID { get; set; }
+        public string PartName { get; set; }
+        public bool IsCritical { get; set; }
+        public int ModuleID { get; set; }
+        public int EngineID { get; set; }
+        public string EPC { get; set; }
+        public Int16 InspectionStatusID { get; set; }
+        public string TrolleyID { get; set; }
+        public Int16 ProcessStatusID { get; set; }
+        public string ExternalVendorName { get; set; }
+        public DateTime ExpectedArrivalDate { get; set; }
+    }
+    public class InspectionStatus
+    {
+        public Int16 InspectionStatusID { get; set; }
+        public string InspectionStatusName { get; set; }
     }
     #endregion
     public class Module_Rec
@@ -84,7 +104,30 @@ namespace EagleServicesWebApp.Models.Main
         //    get { return InspectionStatus != null ? ((InspectionStatus)this.InspectionStatus).ToDescription() : ""; }
         //}
     }
-
+    public class Part_Enquiry
+    {
+        public int PartID { get; set; }
+        public string PartName { get; set; }
+        public string IsCritical { get; set; }
+        public string EPC { get; set; }
+        public string ModuleName { get; set; }
+        public string EngineName { get; set; }
+        public string InspectionStatusName { get; set; }
+        public string ProcessStatusName { get; set; }
+        public string TrolleyName { get; set; }
+        public string TrolleyEPC { get; set; }
+        public Int16? InspectionStatusID { get; set; }
+        public string StorageLocation { get; set; }
+        public string ExternalVendorName { get; set; }
+        public DateTime? ExpectedArrivalDate { get; set; }
+        public string ExpectedArrivalDateValue
+        {
+            get
+            {
+                return ExpectedArrivalDate != null ? ExpectedArrivalDate.Value.ToString("yyyy-MM-dd") :"";
+            }
+        }
+    }
     public class Dashboard_REC
     {
         public int OutModule { get; set; }
@@ -97,20 +140,19 @@ namespace EagleServicesWebApp.Models.Main
     }
     public class MainModel
     {
-        public List<Engine> GetEngineList()
+        public System.Data.Entity.Infrastructure.DbRawSqlQuery<Engine> GetEngineList()
         {
             string sql = "select EngineID,EngineName from [dbo].[tblEngineMaster]";
 
             DatabaseContext db = new DatabaseContext();
             List<SqlParameter> oParameters = new List<SqlParameter>();
             SqlParameter[] vSqlParameter = oParameters.ToArray();
-            var data = db.Database.SqlQuery<Engine>(sql, vSqlParameter).ToList();
+            var data = db.Database.SqlQuery<Engine>(sql, vSqlParameter);
             return data;
-
         }
         public List<Module> GetModuleList()
         {
-            string sql = "select ModuleID,ModuleName, em.EngineName from [dbo].[tblModuleMaster] mm " +
+            string sql = "select ModuleID,ModuleName, em.EngineName, em.EngineID from [dbo].[tblModuleMaster] mm " +
                 "   left join [dbo].[tblEngineMaster] em on em.EngineID=mm.EngineID ";
 
             DatabaseContext db = new DatabaseContext();
@@ -119,6 +161,16 @@ namespace EagleServicesWebApp.Models.Main
             var moduleData = db.Database.SqlQuery<Module>(sql, vSqlParameter).ToList();
             return moduleData;
 
+        }
+        public System.Data.Entity.Infrastructure.DbRawSqlQuery<InspectionStatus> GetInspectionStatusList()
+        {
+            string sql = "select InspectionStatusID,InspectionStatusName from [dbo].[tblInspectionStatus]";
+
+            DatabaseContext db = new DatabaseContext();
+            List<SqlParameter> oParameters = new List<SqlParameter>();
+            SqlParameter[] vSqlParameter = oParameters.ToArray();
+            var data = db.Database.SqlQuery<InspectionStatus>(sql, vSqlParameter);
+            return data;
         }
         public List<Module_Rec> GetModuleData(int engine)
         {
@@ -142,9 +194,17 @@ namespace EagleServicesWebApp.Models.Main
             SqlParameter[] vSqlParameter = oParameters.ToArray();
             var partData = db.Database.SqlQuery<Part_Rec>(sql, vSqlParameter).ToList();
             return partData;
-
         }
+        public System.Data.Entity.Infrastructure.DbRawSqlQuery<Part_Enquiry> GetPartDataByCondition(string query)
+        {
+            string sql = " select * from Enquiry_vw " + query;
 
+            DatabaseContext db = new DatabaseContext();
+            List<SqlParameter> oParameters = new List<SqlParameter>();
+            SqlParameter[] vSqlParameter = oParameters.ToArray();
+            var data = db.Database.SqlQuery<Part_Enquiry>(sql, vSqlParameter);
+            return data;
+        }
         public List<Dashboard_REC> GetDashboardData(int engine)
         {
             try
